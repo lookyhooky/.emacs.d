09adc6e3d78eea103eeaa78dc8808a5d12fea579
John Hooks <john@hookscrane.com>
2016-08-04 15:37:45 -0700

    Improve notes even further and added auto-complete support to ielm

--- init.el
+++ init.el
@@ -333,6 +333,18 @@
 (with-eval-after-load "ispell"
   (setq ispell-program-name "hunspell"))
 
+(defun ielm-auto-complete ()
+  "Enables `auto-complete' support in \\[ielm].
+As found at `https://www.masteringemacs.org/article/evaluating-elisp-emacs'"
+  (setq ac-sources '(ac-source-functions
+                     ac-source-variables
+                     ac-source-features
+                     ac-source-symbols
+                     ac-source-words-in-same-mode-buffers))
+  (add-to-list 'ac-modes 'inferior-emacs-lisp-mode)
+  (auto-complete-mode 1))
+(add-hook 'ielm-mode-hook 'ielm-auto-complete)
+
 ;; Not Working
 ;; (defun comint-clear-buffer ()
 ;;   "Clear the comint buffer."
